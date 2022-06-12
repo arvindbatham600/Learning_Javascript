6094fecc5d5d87094fb86306e631310857f0c831
Arvind Batham <arvindbatham600@gmail.com>
2022-06-12 21:17:08 +0530

    Added some lines of code

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-alert("Hello World");
+// console log
+
+// console.log("hello world");
+// console.warn("this is a warning");
+// console.error("this is a error");
+// ----------------------------------------------------
+// Variables
+
+// Three variables in javascript
+// var,let,const
+
+// const age = 30;
+// age = 21
+// console.log(age)
+// ------------------------------------------------------
+// Data types
+
+// const name = 'arvind';
+// const age = 20;
+// const rating = 2.2;
+// const value = null;
+// const isCool = true;
+// const defination = undefined;
+
+// console.log(typeof(defination))
+//------------------------------------------------------
+// String Concatenaton
+// two ways
+
+// const name = "arvind";
+// const age = 20;
+
+// console.log("hello my name is " + name + "and i am " + age);
+// console.log(`hello my name is ${name} and i am ${age}`)
+// ------------------------------------------------
+// String methods
+
+// const name = "Arvind Batham";
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.substring(0,6).toUpperCase());
+// console.log(name.split(' '));
+//-------------------------------------------------------
+
+// Array
+/*const numbers = new Array(1,2,3,4,5,6); // one way to create Array
+const fruit = new Array("Apple","Mango","Banana", true);
+const randomValue = new Array(1,3,4,"mango");
+
+const fruits = ["apple","banana","mango"]; // Second way to create Array
+console.log(numbers[2]); // Accessing Elements of the Array
+
+fruits[3] = "papaya";
+fruits.push("mangos"); // for deleting the last element of the array
+fruits.unshift("straberries"); // for adding element in the staring
+fruits.pop();
+console.log(Array.isArray(fruits)); // for checking the Array...is fruits is Array or not.
+console.log(fruits.indexOf('banana')); // for cheking the index of the banana.
+console.log(fruits)*/
+
+//
