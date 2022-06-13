4fa6bc63623523a1811d06abf688a655baa9287b
Arvind Batham <arvindbatham600@gmail.com>
2022-06-13 22:30:54 +0530

    added more lines of code

--- index.js
+++ index.js
@@ -59,4 +59,202 @@ console.log(Array.isArray(fruits)); // for checking the Array...is fruits is Arr
 console.log(fruits.indexOf('banana')); // for cheking the index of the banana.
 console.log(fruits)*/
 
-//
+// -------------------------------------------------------------------------------
+/*// Object Literals
+
+const person = {
+  firstname: "Arvind",
+  lastname: "Batham",
+  age: 30,
+  hobbies: ["music", "cricket", "dancing"],
+  address: {
+    colony: "ABC",
+    district: "Gwalior",
+    state: "Madhya Pradesh",
+  },
+};
+console.log(person.hobbies[1]);
+console.log(person.address.state);
+
+person.email = "alex@gmal.com"; // adding email 
+console.log(person.email);
+
+const {
+  firstname,
+  lastname,
+  address: { district },
+} = person;
+console.log(firstname, lastname, district);
+
+// JSON (JavaScript object Notation)
+
+const Person_json = JSON.stringify(person);
+console.log(Person_json);*/
+
+// -------------------------------------------------------------------------------
+
+// Array of object literal
+/*const todos = [
+  {
+    id: 1,
+    text: "learnig javascript",
+    isCompleted: false,
+  },
+
+  {
+    id: 2,
+    text: "meeting with friends",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    text: "going to play",
+    isCompleted: true,
+  },
+];
+
+console.log(todos[1].text);*/
+
+// ----------------------------------------------------
+
+// for loop
+/*
+for (let i = 0; i < 10; i++) {
+  //   console.log(`this is ${i}`);
+}
+
+const todos = [
+  {
+    id: 1,
+    text: "learnig javascript",
+    isCompleted: true,
+  },
+
+  {
+    id: 2,
+    text: "meeting with friends",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    text: "going to play",
+    isCompleted: true,
+  },
+];
+
+  for(let i = 0; i < todos.length; i++){
+    console.log(todos[i].text)
+  }
+
+  for(let todo of todos){
+    console.log(todo.text)
+  }
+
+// foreach, map, filter
+
+todos.forEach(function (todo) {
+  console.log(todo.text);
+});
+
+let todoNewArray = todos.map(function (todo) {
+  return todo.text;
+});
+
+console.log(todoNewArray);
+
+let todo_NewArray = todos
+  .filter(function (todo) {
+    return todo.isCompleted === true;
+  })
+  .map(function (todo) {
+    return todo.text;
+  });
+
+console.log(todo_NewArray);
+
+let p = 0;
+while(p < 10){
+    console.log(p)
+    p++;
+}
+*/
+// ----------------------------------------------------------
+
+// Ternary Operator
+/*
+let x = 11;
+let color = x > 10 ? "blue" : "red";
+console.log(color) */
+
+// ----------------------------------------------------------
+
+// Switch Case
+/*
+let x = 11;
+let color = x > 10 ? "blue" : "red";
+
+switch (color) {
+  case "blue":
+    console.log("color is blue");
+    break;
+  case "red":
+    console.log("color is red");
+    break;
+  default:
+    console.log("No color match");
+}
+*/
+
+// -------------------------------------------------------------
+
+// Function
+/*
+function addNums(n1, n2) {
+  console.log(n1 + n2);
+}
+
+addNums(5, 5);
+
+we can aslo set the default parameters
+function addNums(n1 = 1, n2 = 1) {
+  return n1 + n2;
+}
+
+console.log(addNums());
+
+Arrow Function
+
+const addNums = (n1,n2) => {
+  console.log(n1 + n2);
+};
+
+addNums(5, 6);
+
+todos.forEach((todo) => {
+  todo.text;
+});
+*/
+// -----------------------------------------------------------------
+
+// Class(object oriented programming)
+
+// class Person {
+//   constructor(firstname, lastname, dob) {
+//     this.firstname = firstname;
+//     this.lastname = lastname;
+//     this.dob = new Date(dob);
+//   }
+
+//   getFullName() {
+//     return `${this.firstname} ${this.lastname}`;
+//   }
+
+//   getBirthYear() {
+//     return this.dob.getFullYear();
+//   }
+// }
+
+// const person1 = new Person("Arvind", "Batham", "1-1-2001");
+// const person2 = new Person("Alex", "Anonymous", "2-2-2002");
+// console.log(person1.getBirthYear());
+// console.log(person2.getFullName());
